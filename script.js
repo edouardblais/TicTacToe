8b78c773d2579d874dc10267b25aa72a41e54639
edouard <edouard.blais@hotmail.com>
2022-05-14 18:30:15 -0400

    started winning logic + styling added

--- script.js
+++ script.js
@@ -26,15 +26,35 @@ const Gameflow = (() => {
 
     let activePlayer = player1;
 
+    const winningConfigurations = [
+        [Gameboard.board[0], Gameboard.board[1], Gameboard.board[2]], 
+        [Gameboard.board[3], Gameboard.board[4], Gameboard.board[5]], 
+        [Gameboard.board[6], Gameboard.board[7], Gameboard.board[8]], 
+        [Gameboard.board[0], Gameboard.board[3], Gameboard.board[6]], 
+        [Gameboard.board[1], Gameboard.board[4], Gameboard.board[7]], 
+        [Gameboard.board[2], Gameboard.board[5], Gameboard.board[8]], 
+        [Gameboard.board[0], Gameboard.board[4], Gameboard.board[8]], 
+        [Gameboard.board[2], Gameboard.board[4], Gameboard.board[6]]
+    ];
+
     function toggleActivePlayer() {
         activePlayer = activePlayer == player1? player2:player1;
     };
+
+    function checkWin() {
+        winningConfigurations.forEach((i) => {
+            if (i === [activePlayer.symbol, activePlayer.symbol, activePlayer.symbol]) {
+                console.log('winner!')
+            }
+        })
+    }
 
     const gameboard = document.querySelector('.gameboard');
     Array.from(gameboard.children).forEach((square, index) => {
         square.addEventListener('click', () => {
                 square.classList.add(activePlayer.symbol);
                 Gameboard.board[index] = activePlayer.symbol;
+                checkWin();
                 toggleActivePlayer();
             });
         })
