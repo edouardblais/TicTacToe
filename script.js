9d8032eafc8b77e2f917fbea89d97f41d4748838
edouard <edouard.blais@hotmail.com>
2022-05-16 23:21:52 -0400

    winning and draw and reset logic done

--- script.js
+++ script.js
@@ -25,9 +25,17 @@ const Gameflow = (() => {
     const player2 = createPlayer('Player 2', 'O');
 
     let activePlayer = player1;
+    
+    let winningConfigurations = [];
 
-    const winningConfigurations = [
-        [Gameboard.board[0], Gameboard.board[1], Gameboard.board[2]], 
+    let totalSquares = 9;
+
+    let win = false;
+
+    function updateWinningConfigurations() {
+       winningConfigurations = [];
+       winningConfigurations = [
+        [Gameboard.board[0], Gameboard.board[1], Gameboard.board[2]],
         [Gameboard.board[3], Gameboard.board[4], Gameboard.board[5]], 
         [Gameboard.board[6], Gameboard.board[7], Gameboard.board[8]], 
         [Gameboard.board[0], Gameboard.board[3], Gameboard.board[6]], 
@@ -35,26 +43,56 @@ const Gameflow = (() => {
         [Gameboard.board[2], Gameboard.board[5], Gameboard.board[8]], 
         [Gameboard.board[0], Gameboard.board[4], Gameboard.board[8]], 
         [Gameboard.board[2], Gameboard.board[4], Gameboard.board[6]]
-    ];
+       ];
+    };
 
     function toggleActivePlayer() {
         activePlayer = activePlayer == player1? player2:player1;
     };
 
     function checkWin() {
-        winningConfigurations.forEach((i) => {
-            if (i === [activePlayer.symbol, activePlayer.symbol, activePlayer.symbol]) {
-                console.log('winner!')
-            }
-        })
+        updateWinningConfigurations();
+        for (let i = 0; i <winningConfigurations.length; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (winningConfigurations[i][0] === 'X' && winningConfigurations[i][1] === 'X' && winningConfigurations[i][2] === 'X') {
+                    console.log('Player 1 wins');
+                    win = true;
+                    resetGame()
+                } else if (winningConfigurations[i][0] === 'O' && winningConfigurations[i][1] === 'O' && winningConfigurations[i][2] === 'O') {
+                    console.log('Player 2 wins');
+                    win = true;
+                    resetGame();
+                };
+            };
+        };
+    };
+
+    function checkIfSquareLeft() {
+        if (totalSquares === 0 && win === false) {
+            console.log('Draw!');
+            resetGame();
+        }
     }
 
+    function resetGame() {
+        Array.from(gameboard.children).forEach((square, index) => {
+            square.classList.remove('X');
+            square.classList.remove('O');
+            Gameboard.board[index] = '';
+            totalSquares = 9;
+            win = false;
+        })
+    }
+    
     const gameboard = document.querySelector('.gameboard');
+
     Array.from(gameboard.children).forEach((square, index) => {
         square.addEventListener('click', () => {
                 square.classList.add(activePlayer.symbol);
                 Gameboard.board[index] = activePlayer.symbol;
+                totalSquares = totalSquares - 1;
                 checkWin();
+                checkIfSquareLeft();
                 toggleActivePlayer();
             });
         })
